4671eb4d1d8e4fb90798b7916cde45ac341049f9
√ｲﾶﾑム <113216701+magnusekvojtech@users.noreply.github.com>
2023-02-23 22:43:43 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+int rok;
+Console.WriteLine("\n Zadejte rok:");
+string z_klavesnice = Console.ReadLine();
+rok = Convert.ToInt32(z_klavesnice);
+if ((rok % 4) != 0)
+    Console.WriteLine("Tento rok není přestupný");
+else
+    Console.WriteLine("Tento rok je přestupný");
